41afd16ebff738b9e773f6db760b3b6272fff7b2
DavidB <david.buchner21@imperial.ac.uk>
2025-10-08 16:01:53 +0100

    Added function to load imaging data (.tif files) onto a numpy array (load_dataset.py).  Added Load data section to main_pipeline.py script

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -6,7 +6,6 @@
  Author:         David Buchner, Imperial College London
  Created:        08/10/2025
  Last Modified:  08/10/2025
- Version:        -
  Description:
     Script to execute the image segmentation pipeline.
     The pipline consists of:
@@ -30,5 +29,11 @@
 # -----------------------------
 # Load Python packages
 import numpy as np
+from load_dataset import load_tif_sequence
+
+# -----------------------------
+# 1. Load the 3D image/dataset onto a numpy array
+# i.e. into a format that allows efficient manipulation in the later steps
+stack = load_tif_sequence("../imaging_data/spherical_particles",start=100,end=199)
 
 # -----------------------------
