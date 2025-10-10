712a60dca57c6301875c93c5449851120e9237a1
DavidB <david.buchner21@imperial.ac.uk>
2025-10-10 14:55:17 +0100

    Added script to execute the pipeline using the scikit-image library.

---
+++ src/main_pipelin_libraries.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+===========================================================
+ Script Name:    main_pipeline_libraries.py
+ Author:         David Buchner, Imperial College London
+ Created:        10/10/2025
+ Last Modified:  10/10/2025
+ Description:
+    Script to execute the image segmentation pipeline. It makes use of the image processing library scikit-image
+    The pipline consists of:
+    1. Loading and preprocessing data
+    2. Threshholding and watershed segmentation
+    3. Segmentation labelling
+    4. Visualization segmentation
+
+ Usage:
+    python main_pipeline_libraries.py /path/to/data/
+
+ Requirements:
+    - Python 3.x
+    - Required libraries:
+        * numpy
+        * scikit-image
+
+ Notes:
+
+===========================================================
+'''
+# -----------------------------
+# Load Python packages
+import numpy as np
+from load_dataset import load_tif_sequence
+
+# -----------------------------
+# 1. Load the 3D image/dataset onto a numpy array
+# i.e. into a format that allows efficient manipulation in the later steps
+stack = load_tif_sequence("../imaging_data/spherical_particles",start=100,end=199)
+
+# -----------------------------
