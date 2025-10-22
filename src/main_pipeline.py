c3f6934088801b5ad8154919546950f890ad3be6
DavidB <david.buchner21@imperial.ac.uk>
2025-10-22 17:09:34 +0100

    Implemented numpy based function that calculates the Otsu threshold, and a 3d Chamfer Distance transform (approximates Euclidean distance of each voxel to the nearest boundary, exact calculation found to be computationally to costly).

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -1,40 +1,79 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-===========================================================
- Script Name:    main_pipeline.py
- Author:         David Buchner, Imperial College London
- Created:        08/10/2025
- Last Modified:  08/10/2025
- Description:
-    Script to execute the image segmentation pipeline.
-    The pipline consists of:
-    1. Loading and preprocessing data
-    2. Threshholding and watershed segmentation
-    3. Segmentation labelling
-    4. Visualization segmentation
+"""main_pipeline.py
+Author:         David Buchner, Imperial College London
+Created:        08/10/2025
+Last Modified:  22/10/2025
+Description:
+   Script to execute the image segmentation pipeline.
+   The pipline consists of:
+   1. Loading and preprocessing data
+   2. Threshholding and watershed segmentation
+   3. Segmentation labelling
+   4. Visualization segmentation
 
- Usage:
-    python main_pipeline.py /path/to/data/
+Usage:
+   python main_pipeline.py /path/to/data/
 
- Requirements:
-    - Python 3.x
-    - Required libraries:
-        * numpy
+Requirements:
+   - Python 3.x
+   - Required libraries:
+       * numpy
 
- Notes:
-
-===========================================================
 """
 
 # -----------------------------
 # Load Python packages
 import numpy as np
+
+from image_processing import chamfer_distance_3d, otsu_threshold
 from load_dataset import load_tif_sequence
+from visualisation import plot_3d_orthogonal_planes
 
 # -----------------------------
 # 1. Load the 3D image/dataset onto a numpy array
 # i.e. into a format that allows efficient manipulation in the later steps
-stack = load_tif_sequence("../imaging_data/spherical_particles", start=100, end=199)
+image3d = load_tif_sequence("../imaging_data/spherical_particles", start=100, end=199)
 
 # -----------------------------
+# 2. Crop the image to a cubical section.
+# Get the shape of the original 3D image (e.g., (depth, height, width))
+size_of_original_image3d = np.shape(image3d)
+
+# Determine the size of the cube: use the smallest dimension of the original image
+size_cubecrop = np.min(size_of_original_image3d)
+
+# Crop the 3D image to a centered cube
+image3d_cubecrop = image3d[
+    tuple(
+        slice((s - size_cubecrop) // 2, (s - size_cubecrop) // 2 + size_cubecrop)
+        for s in image3d.shape
+    )
+]
+
+# Print a statement showing the crop
+print(
+    f"3D image cropped from size {size_of_original_image3d} to {image3d_cubecrop.shape}"
+)
+
+# Create orthogonal slice visualization
+# plot_3d_orthogonal_planes(image3d_cubecrop, snapshot_view=(30, 300))
+
+# -----------------------------
+# 4. Apply a thresholding
+# Compute Otsu's threshold
+thresh_value = otsu_threshold(image3d_cubecrop)
+
+# Apply threshold
+binary_cubecrop = image3d_cubecrop < thresh_value  # returns a boolean array
+
+# Create orthogonal slice visualization
+# plot_3d_orthogonal_planes(binary_cubecrop, snapshot_view=(30, 300))
+
+# -----------------------------
+# 5. Apply distance transform and watershed segmentation
+# Compute distance transform
+distance_cubecrop = chamfer_distance_3d(binary_cubecrop)
+distance_cubecrop = distance_cubecrop
+
+# Create orthogonal slice visualization
+plot_3d_orthogonal_planes(distance_cubecrop, snapshot_view=(30, 300))
