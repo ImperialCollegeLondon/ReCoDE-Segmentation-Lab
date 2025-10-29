e3c14f9a32b72b5a983c0e7c69064a07591ad7e0
DavidB <david.buchner21@imperial.ac.uk>
2025-10-29 13:52:26 +0000

    Removed pipeline execution steps from this branch

--- src/main_pipeline.py
+++ src/main_pipeline.py
@@ -59,21 +59,4 @@ print(
 # plot_3d_orthogonal_planes(image3d_cubecrop, snapshot_view=(30, 300))
 
 # -----------------------------
-# 4. Apply a thresholding
-# Compute Otsu's threshold
-thresh_value = otsu_threshold(image3d_cubecrop)
 
-# Apply threshold
-binary_cubecrop = image3d_cubecrop < thresh_value  # returns a boolean array
-
-# Create orthogonal slice visualization
-# plot_3d_orthogonal_planes(binary_cubecrop, snapshot_view=(30, 300))
-
-# -----------------------------
-# 5. Apply distance transform and watershed segmentation
-# Compute distance transform
-distance_cubecrop = chamfer_distance_3d(binary_cubecrop)
-distance_cubecrop = distance_cubecrop
-
-# Create orthogonal slice visualization
-plot_3d_orthogonal_planes(distance_cubecrop, snapshot_view=(30, 300))
